ac2ef9c919eb969470ca5c8b15428ce14583ecc3
seekersoftec <seekersoftec@gmail.com>
2020-08-29 16:04:23 +0100

    [refactoring code]

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ a port of python's fake-user-agent library
 
 
 get the most used browser from w3schools
+https://www.w3schools.com/browsers/default.asp
--- index.js
+++ index.js
@@ -1,38 +1,66 @@
-const scrapUserAgentString = require("./lib/scraper/scraper");
+const { isEmpty, isArray } = require("lodash");
+const UserAgentScraper = require("./lib/scraper/scraper");
 const { genRandomStr } = require("./lib/tools");
-
+// 
 process.env.cache = true;
-
-scrapUserAgentString({
-  enableCache: process.env.cache,
-  cacheName: "page_links_cache",
-  UserAgentStringUrl:
-    "http://www.useragentstring.com/pages/useragentstring.php",
-})
-  .then((linkData) => {
-    let browserList = ["chrome", "opera", "firefox", "edge"]; // default browser list
-    let UAName = genRandomStr(browserList);
+//
+const { genRandomStr } = require("../tools");
+class UserAgent {
+  constructor(browserList = [
+    "chrome",
+    "edge",
+    "firefox",
+    "safari",
+    "opera",
+  ]) {
+    this.browserList = browserList;
+  }
+  get UserAgent() {
+    //
+    let selectedBrowsers = [];
+    //
+    let pageLinks = new UserAgentScraper({
+      enableCache: process.env.cache,
+      cacheName: "page_links_cache",
+      onUpdate: false,
+      userAgentStringUrl:
+        "http://www.useragentstring.com/pages/useragentstring.php",
+    });
+    //
+    let linksArr = await pageLinks.scraper();
+    linksArr.forEach((data) => {
+      if (browserList.includes(data.linkName.toLowerCase())) {
+        selectedBrowsers.push(data);
+      }
+    });
     //
     //
-    if (UAName != "") {
-      linkData.forEach((data) => {
-        if (data.linkName.toLowerCase() == UAName.toLowerCase()) {
-          //
-          scrapUserAgentString({
-            enableCache: process.env.cache,
-            cacheName: UAName + "_UA_cache",
-            UserAgentStringUrl: "http://www.useragentstring.com" + data.link,
-          })
-            .then((UAstrings) => {
-              console.log(genRandomStr(UAstrings));
-            })
-            .catch((error) => {
-              console.error(error);
-            });
-        }
+    console.log(selectedBrowsers);
+    let UAStrings = [];
+    await selectedBrowsers.forEach(async (data) => {
+      //
+      let UAString = new UserAgentScraper({
+        enableCache: process.env.cache,
+        cacheName: data.linkName.toLowerCase() + "_UA_cache",
+        onUpdate: false,
+        UserAgentStringUrl: "http://www.useragentstring.com" + data.link,
       });
-    }
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+      //
+      let UAStringData = await UAString.scraper();
+      // return genRandomStr(UAStringData);
+      UAStrings.push(genRandomStr(UAStringData));
+    });
+    //
+    // return UAStrings;
+    console.log(UAStrings);
+  }
+
+}
+
+//
+// 
+//
+let browserList = ["chrome", "opera"];
+let ua = new UserAgent(browserList);
+
+console.log(ua);
